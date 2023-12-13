725af15677e2e0b81bae02a5b5bd641c12bc8938
Рома <144273650+Mamooin@users.noreply.github.com>
2023-12-13 13:52:37 +0300

    Delete alltested.py

--- alltested.py
+++
@@ -1,7 +0,0 @@
-from circle_test import TestCircleFunctions
-from rectangle_test import TestRectangleFunctions
-from square_test import TestSquareFunctions
-from triangle_test import TestTriangleFunctions
-
-if __name__ == '__main__':
-    main()
